36c9b876f939890a220f58fd203198523be96617
kooksk95@snu.ac.kr <kooksk95@snu.ac.kr>
2024-04-17 16:34:51 +0900

    Add: seed data

--- src/main/java/com/example/account/entity/User.java
+++ src/main/java/com/example/account/entity/User.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 
 @Setter
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO loan (bank_code, name, base_rate, max_spread, limit_amount)
+            VALUES ('004', 'happy loan', '3.5', '4.0', '1000000000'),
+                   ('004', 'star loan', '4.2', '4.0', '1000000000'),
+                   ('004', 'youngs loan', '3.0', '3.1', '1000000000'),
+                   ('020', 'Woori youngs loan', '2.9', '4.1', '1000000000');
+
+INSERT INTO users (name, birth_date) VALUES('Mia kook', '1996-02-27');
